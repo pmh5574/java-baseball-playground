534c6d3a52f24d11711534252f1e45bd98649763
pmh5574 <g_a_m_e_@naver.com>
2023-07-21 14:14:54 +0900

    학습테스트 실습 완료, 문자열 계산기 진행중

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -13,6 +14,8 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SetTest {
 
@@ -32,19 +35,20 @@ class SetTest {
     @Test
     @DisplayName("요구사항 1 - Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트 구현")
     void size() {
-//        numbers.size()
-
-//        numbers.size()
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3, Integer.MAX_VALUE})
-    @DisplayName("ParameterizedTest로 중복 코드 제거하기")
-    void contains() {
-        assertThat(numbers.contains(1)).isTrue();
-        assertThat(numbers.contains(2)).isTrue();
-        assertThat(numbers.contains(3)).isTrue();
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("요구사항 2 - ParameterizedTest로 중복 코드 제거하기")
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
     }
 
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("요구사항 3 - 123일땐 true 45는 false가 반환되는 ")
+    void containsTrueFalse(int number, Boolean expected) {
+        assertEquals(expected, numbers.contains(number));
+    }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,10 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
@@ -40,6 +43,21 @@ class StringTest {
     @DisplayName("abc 값이 주어졌을때 String의 charAt()을 활용해 특정 문자의 위치가 초과되면 StringIndexOutOfBoundsException 발생")
     void charAt() {
 
+        assertThatThrownBy(() -> {
+            char s = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
+
+//        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("들어온 값의 순서에 따라 값 체크")
+    void strCalculator() {
+        Scanner scanner = new Scanner(System.in);
+
+        String value = scanner.nextLine();
         assertThatThrownBy(() -> {
             char s = "abc".charAt(3);
         }).isInstanceOf(IndexOutOfBoundsException.class)
