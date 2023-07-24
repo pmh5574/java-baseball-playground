8ef1ae5c3164b86a8238a9354eb1f6d691053592
pmh5574 <g_a_m_e_@naver.com>
2023-07-24 11:14:41 +0900

    문자열 계산기 완료, BaseballTest 작성중

---
+++ src/test/java/study/BaseballTest.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseballTest {
+
+    @Test
+    @DisplayName("숫자야구게임")
+    void game() {
+
+    }
+}
